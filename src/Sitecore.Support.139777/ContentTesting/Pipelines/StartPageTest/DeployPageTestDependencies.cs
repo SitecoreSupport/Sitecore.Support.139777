b2d5ce584733237950710e51d7fe7044381f1dfb
Sergey Plashenko <spl@sitecore.net>
2018-01-03 16:58:41 +0200

    Initial commit

---
+++ src/Sitecore.Support.139777/ContentTesting/Pipelines/StartPageTest/DeployPageTestDependencies.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Sitecore.ContentTesting.Helpers;
+using Sitecore.ContentTesting.Inspectors;
+using Sitecore.ContentTesting.Pipelines.StartPageTest;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
+using Sitecore.Publishing;
+
+namespace Sitecore.Support.ContentTesting.Pipelines.StartPageTest
+{
+  public class DeployPageTestDependencies : StartPageTestProcessor
+  {
+    public override void Process(StartPageTestArgs args)
+    {
+      Database[] targets = PublishingHelper.GetTargets(args.HostItem).ToArray<Database>();
+      foreach (DataUri uri in new TestVariablesInspector().GetContentTestDataSources(args.Test.PageLevelTestVariables))
+      {
+        Item item = args.HostItem.Database.GetItem(uri);
+        if (item != null)
+        {
+          Language[] languages = new Language[] { args.HostItem.Language };
+          PublishManager.PublishItem(item, targets, languages, false, true);
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.139777/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.139777")]
+[assembly: AssemblyProduct("Sitecore.Support.139777")]
+[assembly: ComVisible(false)]
